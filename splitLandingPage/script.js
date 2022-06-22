69afe9e4cb2db37c958d74ac07558c71bb7f0289
Saugat Poudel <njsaugat@gmail.com>
2022-06-22 07:15:16 +0545

    script file added

---
+++ splitLandingPage/script.js
@@ -0,0 +1,6 @@
+// const left=document.querySelector('.playstation')
+// const right=document.querySelector('.xbox')
+
+// left.addEventListener('mouseenter',()=>{
+//     left.classList.add('show')
+// })
