5551ec32366060acaff27762d9b12a12ab7f0cd4
Saugat Poudel <njsaugat@gmail.com>
2022-06-23 08:10:16 +0545

    script file added

---
+++ choicePicker/script.js
@@ -0,0 +1,43 @@
+// const choice=document.querySelector('.text-choices');
+// const choicesClass=document.body.querySelector('.contain');
+// console.log(choicesClass)
+
+
+// // const para=document.createElement('p')
+// //                 para.innerText='1213fsd;'
+// //                 content.appendChild(para)
+
+// // // const para=document.createElement('p')
+// // para.innerText='1hel';
+// // para.classList.add('choice-list')
+// // console.log(para)
+// // choicesClass.apppendChild(para)
+
+// console.log(choicesClass)
+// let str=''
+// let choices;
+// choice.addEventListener('input',(event)=>{
+//     str=event.target.value
+//     choices=str.split(',')
+
+//     choice.addEventListener('keypress',(event)=>{
+//         if (event.key === "Enter") {
+//             console.log(choices)
+//             event.preventDefault()            
+//             let paras=[];
+//             choices.forEach(choic=>{
+//                 const para=document.createElement('p')
+//                 para.innerText=choic;
+//                 para.classList.add('choice-list')
+//                 console.log(para)
+//                 paras.push(para)
+//                 // document.body.appendChild(para)
+//             })
+//             // choicesClass.appendChild(paras.toString())
+//             console.log(paras[paras.length-1])
+//         }
+        
+//     })
+// })
+
+// // choice.
