10b1350aa75acd820f202a8a465bd564d15d80ba
Saugat Poudel <njsaugat@gmail.com>
2022-06-25 10:59:43 +0545

    script file added

---
+++ stickyNavBar/script.js
@@ -0,0 +1,10 @@
+const navigation=document.body.querySelector('.navigation')
+
+window.addEventListener('scroll',()=>{
+    // if(window.innerHeight<'93vh'){
+    // }
+    // navigation.style.position='fixed';
+    // navigation.style.top='0';
+    // navigation.classList.add('active')
+
+})
