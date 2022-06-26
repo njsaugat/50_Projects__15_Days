3f42c27ce82daf1bc126722dc14ac36ac461cd55
Saugat Poudel <njsaugat@gmail.com>
2022-06-26 07:24:40 +0545

    script file added

---
+++ cssLoader/script.js
@@ -0,0 +1,11 @@
+// const left=document.body.querySelector('.left')
+// const right=document.body.querySelector('.right')
+// let degree=0;
+// window.addEventListener('load',()=>{
+//     setInterval(()=>{
+//         console.log(12)
+//         left.style.transform=`rotate(${degree}deg)`
+//         right.style.transform=`rotate(${degree}deg)`
+//         degree=(degree+10)%360;
+//     },100)
+// })
