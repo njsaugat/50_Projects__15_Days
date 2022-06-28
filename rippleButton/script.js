486076bb7779a8ab37d47a56ec39d457dc2b7302
Saugat Poudel <njsaugat@gmail.com>
2022-06-28 08:40:15 +0545

    script file added

--- rippleButton/script.js
+++ rippleButton/script.js
@@ -1,11 +1,38 @@
-const button=document.body.querySelector('button.ripple')
+const buttons=document.body.querySelectorAll('button.ripple')
 
-button.addEventListener('click',()=>{
-    const span=document.createElement('span')
-    span.classList.add('circle')
-    span.style=`height: 30px;
-    width: 30px;
-    border-radius: 15px;
-    transform: scale(2);`
-    button.appendChild(span)
+// button.addEventListener('click',()=>{
+//     const span=document.createElement('span')
+//     span.classList.add('circle')
+//     span.style=`height: 30px;
+//     width: 30px;
+//     border-radius: 15px;
+//     transform: scale(2);`
+//     button.appendChild(span)
+// })
+
+const ripple=document.body.querySelector('.ripple')
+
+buttons.forEach(button=>{
+    button.addEventListener('click',function(e){
+        const x=e.clientX
+        const y=e.clientY
+
+        const buttonTop=e.target.offsetTop
+        const buttonLeft=e.target.offsetLeft
+
+        const xInside=x-buttonLeft
+        const yInside=y-buttonTop
+
+        const circle=document.createElement('span')
+
+        circle.classList.add('circle')
+        circle.style.top=yInside+'px'
+        circle.style.left=xInside+'px'
+
+        this.appendChild(circle)
+
+        setTimeout(()=>{
+            circle.remove()
+        },500)
+    })
 })
