b0691a33225c3ea82a2eb6c235f5522e8edadbe3
Saugat Poudel <njsaugat@gmail.com>
2022-07-12 08:01:22 +0545

    script file added

---
+++ verifyAccount/script.js
@@ -0,0 +1,17 @@
+const inputs=document.body.querySelectorAll('.code input')
+inputs[0].focus();
+
+inputs.forEach((input,index)=>{
+    input.addEventListener('keydown',function(e){
+        if(e.key>=0 && e.key<=9){
+            inputs[index].value='';
+            setTimeout(()=>{
+                inputs[index+1].focus()              
+            },10)            
+        }else if(e.key==='Backspace'){
+            setTimeout(()=>{
+                inputs[index-1].focus()
+            },10)
+        }
+    })
+})
